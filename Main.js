26b38120d4c199f991604f4f10fa8abc00fb6103
leonemunyao <leonemunyao@gmail.com>
2023-08-05 17:51:01 +0300

    Javascript

--- Main.js
+++ Main.js
@@ -1,16 +1,24 @@
 const apiKey = "c8f1b1b17752b231ca4ce7cf4273f443";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=Nairobi";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-async function checkWeather() {
-   const response = await fetch(apiUrl + `&appid=${apiKey}`);
+
+const searchBox = document.querySelector(".search input");
+const searchButton = document.querySelector(".search button")
+
+async function checkWeather(city) {
+   const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
    var data = await response.json();
    
    console.log(data);
 
    document.querySelector(".city").innerHTML = data.name;
-   document.querySelector(".temperature").innerHTML = data.main.temp;
-   document.querySelector(".humidity").innerHTML = data.main.humidity;
-   document.querySelector(".wind").innerHTML = data.wind.speed;
+   document.querySelector(".temperature").innerHTML = Math.round(data.main.temp) + "°c";
+   document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+   document.querySelector(".wind").innerHTML = data.wind.speed + "km/hr";
 }
 
-checkWeather();
+
+searchButton.addEventListener("click", ()=>{
+    checkWeather(searchBox.value);
+})
+
